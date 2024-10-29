bf2ad3f283fd0ae1813119728be940b1eb1f0d02
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-10-29 18:09:18 +0100

    user form (edit) #3

---
+++ src/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            // ->add('roles', ChoiceType::class, [
+            //     'choices' => [
+            //         'ROLE_USER' => 'user',
+            //         'ROLE_MODERATEUR' => 'moderateur',
+            //     ]
+            // ])
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('image', FileType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
