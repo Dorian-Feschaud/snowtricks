24d06ead6de85eadea7eec1bafff565c4fcb7836
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-11-08 11:09:53 +0100

    edit user #3

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -84,4 +84,22 @@ class UserController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
+    #[Route('/{id}/edit', name: 'app_user_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function edit(User $user, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+    
 }
