4baa6a759e708b38835a3932d606d94ebc073e4b
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-11-22 11:49:28 +0100

    file input #7 #8

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -6,9 +6,11 @@ use App\Entity\Group;
 use App\Entity\Trick;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class TrickType extends AbstractType
 {
@@ -21,6 +23,31 @@ class TrickType extends AbstractType
                 'class' => Group::class,
                 'choice_label' => 'name',
             ])
+            ->add('medias', FileType::class, [
+                'multiple' => true,
+                'required' => false,
+                'label' => 'Images',
+                'mapped' => false,
+                // 'constraints' => [
+                //     new File([
+                //         'maxSize' => '1024k',
+                //         'mimeTypes' => [
+                //             'image/png',
+                //             'image/jpg',
+                //             'image/jpeg',
+                //         ],
+                //         'mimeTypesMessage' => 'Please upload a valid png, jpg or jpeg document',
+                //     ])
+                // ],
+            ])
+            ->get('medias')->addModelTransformer(new CallbackTransformer(
+                function($medias) {
+                    return null;
+                },
+                function($medias) {
+                    return $medias;
+                }
+            ))
         ;
     }
 
