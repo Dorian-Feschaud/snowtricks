45f10f82284191feeb28dbb6238e5848d3a5aa9b
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-11-28 14:15:59 +0100

    add tricks to home #12

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    } 
+
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
-        return $this->render('index.html.twig', [
+        $trickRepository = $this->em->getRepository(Trick::class);
 
+        $tricks = $trickRepository->findBy([], null, 3, null);
+
+        return $this->render('index.html.twig', [
+            'tricks' => $tricks
         ]);
     }
 }
