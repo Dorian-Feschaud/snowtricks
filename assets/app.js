1f7134af68f692e1b6263ffae049072bc30fc21f
DorianFeschaud <dorian.feschaud@gmail.com>
2024-12-18 10:37:31 +0100

    remove dead code

--- assets/app.js
+++ assets/app.js
@@ -14,5 +14,3 @@ import './styles/form.css';
 import './styles/reset-password.css';
 import './styles/user.css';
 import './styles/group.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
